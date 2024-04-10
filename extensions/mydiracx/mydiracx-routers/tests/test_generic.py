d424d81b04a1b5c945111332c362d1b3f122a228
Ruslan <rmashinistov@gmail.com>
2024-04-10 11:27:54 +0200

    Add functional tests

---
+++ extensions/mydiracx/mydiracx-routers/tests/test_generic.py
@@ -0,0 +1,21 @@
+import pytest
+
+pytestmark = pytest.mark.enabled_dependencies(["ConfigSource", "AuthSettings"])
+
+
+@pytest.fixture
+def normal_user_client(client_factory):
+    with client_factory.normal_user() as client:
+        yield client
+
+
+def test_write(normal_user_client):
+    r = test_client.post("/api/mymanager/insert_owner/username")
+    assert r.status_code == 200
+    assert r.json()
+
+def test_read(normal_user_client):
+    r = test_client.get("/api/mymanager/get_owners")
+    assert r.status_code == 200
+    assert r.json()
+    assert r.json()[0] == 'username'
