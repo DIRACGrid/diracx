951b48be6c5b4c1ab9d5a738ae2f4ba7cfc5c4d2
Ruslan <rmashinistov@gmail.com>
2024-04-10 16:03:40 +0200

    fix for the tests

--- extensions/mydiracx/mydiracx-routers/tests/test_generic.py
+++ extensions/mydiracx/mydiracx-routers/tests/test_generic.py
@@ -10,13 +10,13 @@ def normal_user_client(client_factory):
 
 
 def test_write(normal_user_client):
-    r = test_client.post("/api/mymanager/insert_owner/username")
+    r = normal_user_client.post("/api/mymanager/insert_owner/username")
     assert r.status_code == 200
     assert r.json()
 
 
 def test_read(normal_user_client):
-    r = test_client.get("/api/mymanager/get_owners")
+    r = normal_user_client.get("/api/mymanager/get_owners")
     assert r.status_code == 200
     assert r.json()
     assert r.json()[0] == "username"
