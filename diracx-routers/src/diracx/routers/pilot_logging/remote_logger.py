1b3d2c0e5dc434e7eae641e54c9725ce80446481
martynia <janusz.martyniak@googlemail.com>
2024-05-21 18:18:57 +0100

    feat: enable remote pilot logging system

---
+++ diracx-routers/src/diracx/routers/pilot_logging/remote_logger.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+
+from pydantic import BaseModel
+
+from ..dependencies import PilotLogsDB
+from ..fastapi_classes import DiracxRouter
+from ..pilot_logging import logger
+from .access_policies import ActionType, CheckPilotLogsPolicyCallable
+
+router = DiracxRouter()
+
+
+class LogLine(BaseModel):
+    line_no: int
+    line: str
+
+
+class LogMessage(BaseModel):
+    pilot_stamp: str
+    lines: list[LogLine]
+    vo: str
+
+
+@router.post("/")
+async def send_message(
+    data: LogMessage,
+    pilot_logs_db: PilotLogsDB,
+    check_permissions: CheckPilotLogsPolicyCallable,
+):
+    logger.warning(f"Message received '{data}'")
+    await check_permissions(action=ActionType.CREATE, pilot_db=pilot_logs_db)
+
+    pilot_id = 1234  # need to get pilot id from pilot_stamp (via pilot DB)
+
+    docs = []
+    for line in data.lines:
+        docs.append(
+            {
+                "PilotStamp": data.pilot_stamp,
+                "VO": data.vo,
+                "LineNumber": line.line_no,
+                "Message": line.line,
+            }
+        )
+    await pilot_logs_db.bulk_insert(pilot_logs_db.index_name(pilot_id), docs)
+    return data
