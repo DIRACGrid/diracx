d89f51f6760ab66e81a64ab33ea07b297f37088c
natthan-pigoux <natthan.pigoux@gmail.com>
2025-01-14 17:41:49 +0100

    fix: parse queries in url to get branch and ensure checkout

--- diracx-core/tests/test_config_source.py
+++ diracx-core/tests/test_config_source.py
@@ -9,7 +9,8 @@ from diracx.core.config import ConfigSource, RemoteGitConfigSource
 from diracx.core.config.schema import Config
 
 # The diracx-chart contains a CS example
-TEST_REPO = "git+https://github.com/DIRACGrid/diracx-charts/"
+TEST_REPO = "git+https://github.com/DIRACGrid/diracx-charts.git"
+TEST_REPO_SPECIFIC_BRANCH = TEST_REPO + "?branch=master"
 
 
 def github_is_down():
@@ -22,13 +23,14 @@ def github_is_down():
 
 
 @pytest.mark.skipif(github_is_down(), reason="Github unavailble")
-def test_remote_git_config_source(monkeypatch):
+@pytest.mark.parametrize("repo_url", [TEST_REPO, TEST_REPO_SPECIFIC_BRANCH])
+def test_remote_git_config_source(monkeypatch, repo_url):
 
     monkeypatch.setattr(
         "diracx.core.config.sources.DEFAULT_CONFIG_FILE",
         "k3s/examples/cs.yaml",
     )
-    remote_conf = ConfigSource.create_from_url(backend_url=TEST_REPO)
+    remote_conf = ConfigSource.create_from_url(backend_url=repo_url)
     assert isinstance(remote_conf, RemoteGitConfigSource)
 
     hexsha, modified = remote_conf.latest_revision()
