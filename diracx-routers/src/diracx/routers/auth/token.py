a80f064ab7308f70737ed64b53770ef8a9d0e560
Chris Burr <chrisburr@users.noreply.github.com>
2025-01-27 12:13:43 +0100

    Improve legacy-exchange docs

--- diracx-routers/src/diracx/routers/auth/token.py
+++ diracx-routers/src/diracx/routers/auth/token.py
@@ -287,7 +287,8 @@ async def legacy_exchange(
         import hashlib
         token = secrets.token_bytes()
 
-        # This is the secret to include in the request
+        # This is the secret to include in the request by setting the
+        # /DiracX/LegacyExchangeApiKey CS option in your legacy DIRAC installation
         print(f"API key is diracx:legacy:{base64.urlsafe_b64encode(token).decode()}")
 
         # This is the environment variable to set on the DiracX server
