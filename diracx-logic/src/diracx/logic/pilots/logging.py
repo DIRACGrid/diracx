b075b8a87fc89da9713fb52708e7d0958edb7fe4
martynia <janusz.martyniak@googlemail.com>
2025-03-07 16:20:00 +0000

    feat: refactor pilot logging to include a business logic layer

---
+++ diracx-logic/src/diracx/logic/pilots/logging.py
@@ -0,0 +1,69 @@
+from __future__ import annotations
+
+import logging
+
+from diracx.core.models import LogMessage, ScalarSearchOperator, ScalarSearchSpec
+from diracx.db.os.pilot_logs import PilotLogsDB, bulk_insert, search_message
+from diracx.db.sql.pilot_agents.db import PilotAgentsDB
+
+logger = logging.getLogger(__name__)
+
+
+async def send_message(
+    data: LogMessage,
+    pilot_logs_db: PilotLogsDB,
+    pilot_agents_db: PilotAgentsDB,
+) -> int:
+
+    # get the pilot ID corresponding to a given pilot stamp, expecting exactly one row:
+    search_params = ScalarSearchSpec(
+        parameter="PilotStamp",
+        operator=ScalarSearchOperator.EQUAL,
+        value=data.pilot_stamp,
+    )
+
+    total, result = await pilot_agents_db.search(
+        ["PilotID", "VO", "SubmissionTime"], [search_params], []
+    )
+    if total != 1:
+        logger.error(
+            "Cannot determine PilotID for requested PilotStamp: %r, (%d candidates)",
+            data.pilot_stamp,
+            total,
+        )
+        raise Exception(f"Number of rows !=1 {total}")
+
+    pilot_id, vo, submission_time = (
+        result[0]["PilotID"],
+        result[0]["VO"],
+        result[0]["SubmissionTime"],
+    )
+    docs = []
+    for line in data.lines:
+        docs.append(
+            {
+                "PilotStamp": data.pilot_stamp,
+                "PilotID": pilot_id,
+                "SubmissionTime": submission_time,
+                "VO": vo,
+                "LineNumber": line.line_no,
+                "Message": line.line,
+            }
+        )
+    # bulk insert pilot logs to OpenSearch DB:
+    await bulk_insert(pilot_logs_db, docs, pilot_id)
+    return pilot_id
+
+
+async def get_logs(
+    pilot_id: int,
+    db: PilotLogsDB,
+) -> list[dict]:
+
+    search_params = [{"parameter": "PilotID", "operator": "eq", "value": pilot_id}]
+
+    result = await search_message(db, search_params)
+
+    if not result:
+        return [{"Message": f"No logs for pilot ID = {pilot_id}"}]
+    return result
--- diracx-routers/src/diracx/routers/pilots/logging.py
+++ diracx-routers/src/diracx/routers/pilots/logging.py
@@ -3,9 +3,10 @@ from __future__ import annotations
 import logging
 
 from fastapi import HTTPException, status
-from pydantic import BaseModel
 
-from diracx.core.models import ScalarSearchOperator, ScalarSearchSpec
+from diracx.core.models import LogMessage
+from diracx.logic.pilots.logging import get_logs as get_logs_bl
+from diracx.logic.pilots.logging import send_message as send_message_bl
 
 from ..access_policies import open_access
 from ..dependencies import PilotAgentsDB, PilotLogsDB
@@ -14,22 +15,6 @@ from .access_policies import ActionType, CheckPilotLogsPolicyCallable
 
 logger = logging.getLogger(__name__)
 router = DiracxRouter()
-
-
-class LogLine(BaseModel):
-    line_no: int
-    line: str
-
-
-class LogMessage(BaseModel):
-    pilot_stamp: str
-    lines: list[LogLine]
-    vo: str
-
-
-class DateRange(BaseModel):
-    min: str | None = None  # expects a string in ISO 8601 ("%Y-%m-%dT%H:%M:%S.%f%z")
-    max: str | None = None  # expects a string in ISO 8601 ("%Y-%m-%dT%H:%M:%S.%f%z")
 
 
 @open_access
@@ -41,55 +26,11 @@ async def send_message(
     # user_info: Annotated[AuthorizedUserInfo, Depends(verify_dirac_access_token)],
 ) -> int:
 
-    # expecting exactly one row:
-    search_params = ScalarSearchSpec(
-        parameter="PilotStamp",
-        operator=ScalarSearchOperator.EQUAL,
-        value=data.pilot_stamp,
-    )
-
-    total, result = await pilot_agents_db.search(
-        ["PilotID", "VO", "SubmissionTime"], [search_params], []
-    )
-    if total != 1:
-        logger.error(
-            "Cannot determine PilotID for requested PilotStamp: %r, (%d candidates)",
-            data.pilot_stamp,
-            total,
-        )
-        raise HTTPException(
-            status.HTTP_400_BAD_REQUEST, detail=f"Number of rows !=1: {total}"
-        )
-    pilot_id, vo, submission_time = (
-        result[0]["PilotID"],
-        result[0]["VO"],
-        result[0]["SubmissionTime"],
-    )
-
-    # await check_permissions(action=ActionType.CREATE, pilot_agent_db, pilot_id),
-
-    docs = []
-    for line in data.lines:
-        docs.append(
-            {
-                "PilotStamp": data.pilot_stamp,
-                "PilotID": pilot_id,
-                "SubmissionTime": submission_time,
-                "VO": vo,
-                "LineNumber": line.line_no,
-                "Message": line.line,
-            }
-        )
-    await pilot_logs_db.bulk_insert(pilot_logs_db.index_name(pilot_id), docs)
-    """
-    search_params = [{"parameter": "PilotID", "operator": "eq", "value": pilot_id}]
-
-    result = await pilot_logs_db.search(
-        ["Message"],
-        search_params,
-        [{"parameter": "LineNumber", "direction": "asc"}],
-    )
-    """
+    # await check_permissions(action=ActionType.CREATE, pilot_agent_db, pilot_id)
+    try:
+        pilot_id = await send_message_bl(data, pilot_logs_db, pilot_agents_db)
+    except Exception as exc:
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, detail=str(exc)) from exc
     return pilot_id
 
 
@@ -107,13 +48,4 @@ async def get_logs(
         action=ActionType.QUERY, pilot_agents_db=pilot_agents_db, pilot_id=pilot_id
     )
 
-    search_params = [{"parameter": "PilotID", "operator": "eq", "value": pilot_id}]
-
-    result = await db.search(
-        ["Message"],
-        search_params,
-        [{"parameter": "LineNumber", "direction": "asc"}],
-    )
-    if not result:
-        return [{"Message": f"No logs for pilot ID = {pilot_id}"}]
-    return result
+    return await get_logs_bl(pilot_id, db)
