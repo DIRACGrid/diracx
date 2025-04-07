9e00d11379533488a92f1969158274d0554b627e
Robin Van de Merghel <robin.van.de.merghel@cern.ch>
2025-04-07 16:25:14 +0200

    fix: Generating client with autorest

--- extensions/gubbins/gubbins-client/src/gubbins/client/_generated/operations/_operations.py
+++ extensions/gubbins/gubbins-client/src/gubbins/client/_generated/operations/_operations.py
@@ -263,6 +263,31 @@ def build_auth_complete_authorization_flow_request(  # pylint: disable=name-too-
     )
 
 
+def build_auth_pilot_login_request(
+    *, pilot_job_reference: str, pilot_secret: str, **kwargs: Any
+) -> HttpRequest:
+    _headers = case_insensitive_dict(kwargs.pop("headers", {}) or {})
+    _params = case_insensitive_dict(kwargs.pop("params", {}) or {})
+
+    accept = _headers.pop("Accept", "application/json")
+
+    # Construct URL
+    _url = "/api/auth/pilot-login"
+
+    # Construct parameters
+    _params["pilot_job_reference"] = _SERIALIZER.query(
+        "pilot_job_reference", pilot_job_reference, "str"
+    )
+    _params["pilot_secret"] = _SERIALIZER.query("pilot_secret", pilot_secret, "str")
+
+    # Construct headers
+    _headers["Accept"] = _SERIALIZER.header("accept", accept, "str")
+
+    return HttpRequest(
+        method="POST", url=_url, params=_params, headers=_headers, **kwargs
+    )
+
+
 def build_config_serve_config_request(
     *,
     if_modified_since: Optional[str] = None,
@@ -689,6 +714,20 @@ def build_lollygag_get_gubbins_secrets_request(
 
     # Construct URL
     _url = "/api/lollygag/gubbins_sensei"
+
+    # Construct headers
+    _headers["Accept"] = _SERIALIZER.header("accept", accept, "str")
+
+    return HttpRequest(method="GET", url=_url, headers=_headers, **kwargs)
+
+
+def build_pilots_get_pilot_info_request(**kwargs: Any) -> HttpRequest:
+    _headers = case_insensitive_dict(kwargs.pop("headers", {}) or {})
+
+    accept = _headers.pop("Accept", "application/json")
+
+    # Construct URL
+    _url = "/api/pilots/info"
 
     # Construct headers
     _headers["Accept"] = _SERIALIZER.header("accept", accept, "str")
@@ -1426,6 +1465,65 @@ class AuthOperations:  # pylint: disable=abstract-class-instantiated
         _request = build_auth_complete_authorization_flow_request(
             code=code,
             state=state,
+            headers=_headers,
+            params=_params,
+        )
+        _request.url = self._client.format_url(_request.url)
+
+        _stream = False
+        pipeline_response: PipelineResponse = (
+            self._client._pipeline.run(  # pylint: disable=protected-access
+                _request, stream=_stream, **kwargs
+            )
+        )
+
+        response = pipeline_response.http_response
+
+        if response.status_code not in [200]:
+            map_error(
+                status_code=response.status_code, response=response, error_map=error_map
+            )
+            raise HttpResponseError(response=response)
+
+        deserialized = self._deserialize("object", pipeline_response.http_response)
+
+        if cls:
+            return cls(pipeline_response, deserialized, {})  # type: ignore
+
+        return deserialized  # type: ignore
+
+    @distributed_trace
+    def pilot_login(
+        self, *, pilot_job_reference: str, pilot_secret: str, **kwargs: Any
+    ) -> Any:
+        """Pilot Login.
+
+        Endpoint without policy, the pilot uses only its secret.
+
+        :keyword pilot_job_reference: Required.
+        :paramtype pilot_job_reference: str
+        :keyword pilot_secret: Required.
+        :paramtype pilot_secret: str
+        :return: any
+        :rtype: any
+        :raises ~azure.core.exceptions.HttpResponseError:
+        """
+        error_map: MutableMapping = {
+            401: ClientAuthenticationError,
+            404: ResourceNotFoundError,
+            409: ResourceExistsError,
+            304: ResourceNotModifiedError,
+        }
+        error_map.update(kwargs.pop("error_map", {}) or {})
+
+        _headers = kwargs.pop("headers", {}) or {}
+        _params = kwargs.pop("params", {}) or {}
+
+        cls: ClsType[Any] = kwargs.pop("cls", None)
+
+        _request = build_auth_pilot_login_request(
+            pilot_job_reference=pilot_job_reference,
+            pilot_secret=pilot_secret,
             headers=_headers,
             params=_params,
         )
@@ -3071,6 +3169,85 @@ class LollygagOperations:
         cls: ClsType[Any] = kwargs.pop("cls", None)
 
         _request = build_lollygag_get_gubbins_secrets_request(
+            headers=_headers,
+            params=_params,
+        )
+        _request.url = self._client.format_url(_request.url)
+
+        _stream = False
+        pipeline_response: PipelineResponse = (
+            self._client._pipeline.run(  # pylint: disable=protected-access
+                _request, stream=_stream, **kwargs
+            )
+        )
+
+        response = pipeline_response.http_response
+
+        if response.status_code not in [200]:
+            map_error(
+                status_code=response.status_code, response=response, error_map=error_map
+            )
+            raise HttpResponseError(response=response)
+
+        deserialized = self._deserialize("object", pipeline_response.http_response)
+
+        if cls:
+            return cls(pipeline_response, deserialized, {})  # type: ignore
+
+        return deserialized  # type: ignore
+
+
+class PilotsOperations:
+    """
+    .. warning::
+        **DO NOT** instantiate this class directly.
+
+        Instead, you should access the following operations through
+        :class:`~_generated.Dirac`'s
+        :attr:`pilots` attribute.
+    """
+
+    models = _models
+
+    def __init__(self, *args, **kwargs):
+        input_args = list(args)
+        self._client: PipelineClient = (
+            input_args.pop(0) if input_args else kwargs.pop("client")
+        )
+        self._config: DiracConfiguration = (
+            input_args.pop(0) if input_args else kwargs.pop("config")
+        )
+        self._serialize: Serializer = (
+            input_args.pop(0) if input_args else kwargs.pop("serializer")
+        )
+        self._deserialize: Deserializer = (
+            input_args.pop(0) if input_args else kwargs.pop("deserializer")
+        )
+
+    @distributed_trace
+    def get_pilot_info(self, **kwargs: Any) -> Any:
+        """Get Pilot Info.
+
+        Get Pilot Info.
+
+        :return: any
+        :rtype: any
+        :raises ~azure.core.exceptions.HttpResponseError:
+        """
+        error_map: MutableMapping = {
+            401: ClientAuthenticationError,
+            404: ResourceNotFoundError,
+            409: ResourceExistsError,
+            304: ResourceNotModifiedError,
+        }
+        error_map.update(kwargs.pop("error_map", {}) or {})
+
+        _headers = kwargs.pop("headers", {}) or {}
+        _params = kwargs.pop("params", {}) or {}
+
+        cls: ClsType[Any] = kwargs.pop("cls", None)
+
+        _request = build_pilots_get_pilot_info_request(
             headers=_headers,
             params=_params,
         )
