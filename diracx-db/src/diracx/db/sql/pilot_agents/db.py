5f84fd974e9f6229404da04e8d8793288c635108
Robin Van de Merghel <robin.van.de.merghel@cern.ch>
2025-04-07 17:57:49 +0200

    feat: Authorized pilots to access jobs endpoints (+7 squashed commits) Squashed commits: [da5eb05] fix: Fixed client and configuration [0256c34] refactor: Splitting the job section to have only pilots or only users [d00e224] fix: Fixed client (+2 squashed commits) Squashed commits: [855dd32] fix: Fixed client [7701995] fix: Fixed tests: had list instead of set + optimize [93be233] feat: Returning the forbidden jobs in the error [d15825e] test: Adding tests to the WMS access policy, and some fixes [f8bc670] feat: Checking if a pilot has the rights to access a list of jobs [c79aeee] feat: Improving wmsjobpolicy to contain pilots

--- diracx-db/src/diracx/db/sql/pilot_agents/db.py
+++ diracx-db/src/diracx/db/sql/pilot_agents/db.py
@@ -2,10 +2,10 @@ from __future__ import annotations
 
 from datetime import datetime, timezone
 
-from sqlalchemy import insert
+from sqlalchemy import insert, select
 
 from ..utils import BaseSQLDB
-from .schema import PilotAgents, PilotAgentsDBBase
+from .schema import JobToPilotMapping, PilotAgents, PilotAgentsDBBase
 
 
 class PilotAgentsDB(BaseSQLDB):
@@ -43,4 +43,38 @@ class PilotAgentsDB(BaseSQLDB):
         # Insert multiple rows in a single execute call
         stmt = insert(PilotAgents).values(values)
         await self.conn.execute(stmt)
-        return
+
+    async def get_pilot_by_reference(self, pilot_ref: str) -> dict:
+        stmt = select(PilotAgents).where(PilotAgents.pilot_job_reference == pilot_ref)
+
+        # Execute the query and fetch one result
+        result = await self.conn.execute(stmt)
+
+        # Get the first row, which should be the pilot if it exists
+        pilot = result.one()
+
+        return dict(pilot._mapping)
+
+    async def get_pilot_job_ids(self, pilot_id: set) -> list[int]:
+        stmt = select(JobToPilotMapping.job_id).where(
+            JobToPilotMapping.pilot_id == pilot_id
+        )
+
+        # Execute the results
+        result = await self.conn.execute(stmt)
+
+        return list(result.scalars().all())
+
+    async def associate_pilot_with_jobs(self, pilot_id: int, job_ids: list[int]):
+
+        now = datetime.now(tz=timezone.utc)
+
+        # Prepare the list of dictionaries for bulk insertion
+        values = [
+            {"PilotID": pilot_id, "JobID": job_id, "StartTime": now}
+            for job_id in job_ids
+        ]
+
+        # Insert multiple rows in a single execute call
+        stmt = insert(JobToPilotMapping).values(values)
+        await self.conn.execute(stmt)
