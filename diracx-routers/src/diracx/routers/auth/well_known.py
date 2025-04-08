09298fed9968a1fc083b5ccb255e360d953623ab
Christophe Haen <christophe.haen@cern.ch>
2025-04-08 11:14:26 +0200

    Cache static routes

--- diracx-routers/src/diracx/routers/auth/well_known.py
+++ diracx-routers/src/diracx/routers/auth/well_known.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from cachetools import TTLCache
 from fastapi import Request
 
 from diracx.core.models import Metadata, OpenIDConfiguration
@@ -16,6 +17,9 @@ from ..fastapi_classes import DiracxRouter
 router = DiracxRouter(require_auth=False, path_root="")
 
 
+_static_cache: TTLCache = TTLCache(maxsize=5, ttl=60)
+
+
 @router.get("/openid-configuration")
 async def get_openid_configuration(
     request: Request,
@@ -23,14 +27,17 @@ async def get_openid_configuration(
     settings: AuthSettings,
 ) -> OpenIDConfiguration:
     """OpenID Connect discovery endpoint."""
-    return await get_openid_configuration_bl(
-        str(request.url_for("get_oidc_token")),
-        str(request.url_for("userinfo")),
-        str(request.url_for("initiate_authorization_flow")),
-        str(request.url_for("initiate_device_flow")),
-        config,
-        settings,
-    )
+    cached_hash = f"openid-configuration:{config._hexsha}"
+    if cached_hash not in _static_cache:
+        _static_cache[cached_hash] = await get_openid_configuration_bl(
+            str(request.url_for("get_oidc_token")),
+            str(request.url_for("userinfo")),
+            str(request.url_for("initiate_authorization_flow")),
+            str(request.url_for("initiate_device_flow")),
+            config,
+            settings,
+        )
+    return _static_cache[cached_hash]
 
 
 @router.get("/dirac-metadata")
@@ -38,4 +45,8 @@ async def get_installation_metadata(
     config: Config,
 ) -> Metadata:
     """Get metadata about the dirac installation."""
-    return await get_installation_metadata_bl(config)
+    cached_hash = f"dirac-metadata:{config._hexsha}"
+    if cached_hash not in _static_cache:
+        _static_cache[cached_hash] = await get_installation_metadata_bl(config)
+
+    return _static_cache[cached_hash]
