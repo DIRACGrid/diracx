9067ee10562ae17d2323c80ef98abe37f5726b34
Robin Van de Merghel <robin.van.de.merghel@cern.ch>
2025-04-11 13:22:09 +0200

    fix: Fixed the tests and autorest

--- diracx-client/src/diracx/client/_generated/aio/_client.py
+++ diracx-client/src/diracx/client/_generated/aio/_client.py
@@ -19,7 +19,6 @@ from .operations import (
     AuthOperations,
     ConfigOperations,
     JobsOperations,
-    LollygagOperations,
     PilotsOperations,
     WellKnownOperations,
 )
@@ -36,8 +35,6 @@ class Dirac:  # pylint: disable=client-accepts-api-version-keyword
     :vartype config: _generated.aio.operations.ConfigOperations
     :ivar jobs: JobsOperations operations
     :vartype jobs: _generated.aio.operations.JobsOperations
-    :ivar lollygag: LollygagOperations operations
-    :vartype lollygag: _generated.aio.operations.LollygagOperations
     :ivar pilots: PilotsOperations operations
     :vartype pilots: _generated.aio.operations.PilotsOperations
     :keyword endpoint: Service URL. Required. Default value is "".
@@ -89,9 +86,6 @@ class Dirac:  # pylint: disable=client-accepts-api-version-keyword
             self._client, self._config, self._serialize, self._deserialize
         )
         self.jobs = JobsOperations(
-            self._client, self._config, self._serialize, self._deserialize
-        )
-        self.lollygag = LollygagOperations(
             self._client, self._config, self._serialize, self._deserialize
         )
         self.pilots = PilotsOperations(
