e33c7c353eec4c421b5623f8106b467e26d20151
Robin Van de Merghel <robin.van.de.merghel@cern.ch>
2025-06-13 13:41:30 +0200

    feat: Add pilot management: create/delete/patch and query

---
+++ diracx-routers/src/diracx/routers/pilots/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+import logging
+
+from ..fastapi_classes import DiracxRouter
+from .management import router as management_router
+from .query import router as query_router
+
+logger = logging.getLogger(__name__)
+
+router = DiracxRouter()
+router.include_router(management_router)
+router.include_router(query_router)
