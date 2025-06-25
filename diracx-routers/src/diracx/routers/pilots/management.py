4ab77c247ec6667bbcc7f708fa68e73732b01c64
Robin Van de Merghel <robin.van.de.merghel@cern.ch>
2025-06-25 11:32:00 +0200

    fix: Lot of fixes, refactoring tests, and modifying access policy to support DIRAC

--- diracx-routers/src/diracx/routers/pilots/management.py
+++ diracx-routers/src/diracx/routers/pilots/management.py
@@ -18,8 +18,11 @@ from diracx.logic.pilots.management import (
     add_jobs_to_pilot as add_jobs_to_pilot_bl,
 )
 from diracx.logic.pilots.management import (
-    clear_pilots_bulk,
-    delete_pilots_by_stamps_bulk,
+    clear_pilots as clear_pilots_bl,
+)
+from diracx.logic.pilots.management import (
+    delete_pilots_by_stamps,
+    get_pilot_jobs_ids_by_stamp,
     register_new_pilots,
     update_pilots_fields,
 )
@@ -29,7 +32,6 @@ from ..fastapi_classes import DiracxRouter
 from ..utils.users import AuthorizedUserInfo, verify_dirac_access_token
 from .access_policies import (
     ActionType,
-    CheckDiracServicesPolicyCallable,
     CheckPilotManagementPolicyCallable,
 )
 
@@ -38,7 +40,7 @@ router = DiracxRouter()
 logger = logging.getLogger(__name__)
 
 
-@router.post("/management/pilot")
+@router.post("/")
 async def add_pilot_stamps(
     pilot_db: PilotAgentsDB,
     pilot_stamps: Annotated[
@@ -52,16 +54,24 @@ async def add_pilot_stamps(
     user_info: Annotated[AuthorizedUserInfo, Depends(verify_dirac_access_token)],
     check_permissions: CheckPilotManagementPolicyCallable,
     grid_type: Annotated[str, Body(description="Grid type of the pilots.")] = "Dirac",
+    grid_site: Annotated[str, Body(description="Pilots grid site.")] = "Unknown",
+    destination_site: Annotated[
+        str, Body(description="Pilots destination site.")
+    ] = "NotAssigned",
     pilot_references: Annotated[
         dict[str, str] | None,
         Body(description="Association of a pilot reference with a pilot stamp."),
     ] = None,
+    status_reason: Annotated[
+        str, Body(description="Status reason of the pilots.")
+    ] = "Unknown",
 ):
     """Endpoint where a you can create pilots with their references.
 
     If a pilot stamp already exists, it will block the insertion.
     """
-    await check_permissions(action=ActionType.CREATE_PILOT, vo=vo)
+    # TODO: Verify that grid types, sites, destination sites, etc. are valids
+    await check_permissions(action=ActionType.MANAGE_PILOTS)
 
     try:
         await register_new_pilots(
@@ -69,7 +79,10 @@ async def add_pilot_stamps(
             pilot_stamps=pilot_stamps,
             vo=vo,
             grid_type=grid_type,
+            grid_site=grid_site,
+            destination_site=destination_site,
             pilot_job_references=pilot_references,
+            status_reason=status_reason,
         )
 
         # Logs credentials creation
@@ -80,7 +93,7 @@ async def add_pilot_stamps(
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=str(e)) from e
 
 
-@router.delete("/management/pilot", status_code=HTTPStatus.NO_CONTENT)
+@router.delete("/", status_code=HTTPStatus.NO_CONTENT)
 async def delete_pilots(
     pilot_stamps: Annotated[
         list[str], Query(description="Stamps of the pilots we want to delete.")
@@ -93,13 +106,11 @@ async def delete_pilots(
     If at least one pilot is not found, it WILL rollback.
     """
     await check_permissions(
-        action=ActionType.CHANGE_PILOT_FIELD,
-        pilot_stamps=pilot_stamps,
-        pilot_db=pilot_db,
+        action=ActionType.MANAGE_PILOTS,
     )
 
     try:
-        await delete_pilots_by_stamps_bulk(pilot_db=pilot_db, pilot_stamps=pilot_stamps)
+        await delete_pilots_by_stamps(pilot_db=pilot_db, pilot_stamps=pilot_stamps)
     except PilotNotFoundError as e:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
@@ -119,7 +130,7 @@ async def clear_pilots(
             )
         ),
     ],
-    check_permissions: CheckDiracServicesPolicyCallable,
+    check_permissions: CheckPilotManagementPolicyCallable,
     delete_only_aborted: Annotated[
         bool,
         Query(
@@ -132,7 +143,7 @@ async def clear_pilots(
     ] = False,
 ):
     """Endpoint for DIRAC to delete all pilots that lived more than age_in_days."""
-    await check_permissions()
+    await check_permissions(ActionType.MANAGE_PILOTS)
 
     if age_in_days < 0:
         raise HTTPException(
@@ -140,7 +151,7 @@ async def clear_pilots(
             detail="age_in_days must be positive.",
         )
 
-    await clear_pilots_bulk(
+    await clear_pilots_bl(
         pilot_db=pilot_db,
         age_in_days=age_in_days,
         delete_only_aborted=delete_only_aborted,
@@ -170,7 +181,7 @@ EXAMPLE_UPDATE_FIELDS = {
 }
 
 
-@router.patch("/management/pilot", status_code=HTTPStatus.NO_CONTENT)
+@router.patch("/metadata", status_code=HTTPStatus.NO_CONTENT)
 async def update_pilot_fields(
     pilot_stamps_to_fields_mapping: Annotated[
         list[PilotFieldsMapping],
@@ -187,15 +198,8 @@ async def update_pilot_fields(
 
     Note: Only the fields in PilotFieldsMapping are mutable, except for the PilotStamp.
     """
-    # TODO: Add an example for openapi
-    pilot_stamps = [mapping.PilotStamp for mapping in pilot_stamps_to_fields_mapping]
-
     # Ensures stamps validity
-    await check_permissions(
-        action=ActionType.CHANGE_PILOT_FIELD,
-        pilot_db=pilot_db,
-        pilot_stamps=pilot_stamps,
-    )
+    await check_permissions(action=ActionType.MANAGE_PILOTS)
 
     await update_pilots_fields(
         pilot_db=pilot_db,
@@ -203,17 +207,44 @@ async def update_pilot_fields(
     )
 
 
-@router.patch("/management/jobs", status_code=HTTPStatus.NO_CONTENT)
+@router.get("/jobs")
+async def get_pilot_jobs(
+    pilot_db: PilotAgentsDB,
+    pilot_stamp: Annotated[str, Body(description="The stamp of the pilot.")],
+    check_permissions: CheckPilotManagementPolicyCallable,
+) -> list[int]:
+    """Endpoint only for DIRAC services, to get jobs of a pilot."""
+    # FIXME: To be tested
+    await check_permissions(action=ActionType.READ_PILOT_FIELDS)
+
+    try:
+        return await get_pilot_jobs_ids_by_stamp(
+            pilot_db=pilot_db,
+            pilot_stamp=pilot_stamp,
+        )
+    except PilotNotFoundError as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="This pilot does not exist."
+        ) from e
+    except PilotAlreadyAssociatedWithJobError as e:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="This pilot is already associated with this job.",
+        ) from e
+
+
+@router.patch("/jobs", status_code=HTTPStatus.NO_CONTENT)
 async def add_jobs_to_pilot(
     pilot_db: PilotAgentsDB,
     pilot_stamp: Annotated[str, Body(description="The stamp of the pilot.")],
     pilot_jobs_ids: Annotated[
         list[int], Body(description="The jobs we want to add to the pilot.")
     ],
-    check_permissions: CheckDiracServicesPolicyCallable,
+    check_permissions: CheckPilotManagementPolicyCallable,
 ):
     """Endpoint only for DIRAC services, to associate a pilot with a job."""
-    await check_permissions()
+    # FIXME: To be tested
+    await check_permissions(ActionType.MANAGE_PILOTS)
 
     try:
         await add_jobs_to_pilot_bl(
