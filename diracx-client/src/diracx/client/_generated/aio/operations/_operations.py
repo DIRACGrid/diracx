9f79a935e5de97c1c6c9fee5d11391bb396ebae0
Federico Stagni <federico.stagni@cern.ch>
2025-07-02 10:27:26 +0200

    fix: regenerate client

--- diracx-client/src/diracx/client/_generated/aio/operations/_operations.py
+++ diracx-client/src/diracx/client/_generated/aio/operations/_operations.py
@@ -56,6 +56,7 @@ from ...operations._operations import (
     build_well_known_get_installation_metadata_request,
     build_well_known_get_jwks_request,
     build_well_known_get_openid_configuration_request,
+    build_well_known_get_security_txt_request,
 )
 from .._configuration import DiracConfiguration
 
@@ -217,6 +218,53 @@ class WellKnownOperations:
             raise HttpResponseError(response=response)
 
         deserialized = self._deserialize("Metadata", pipeline_response.http_response)
+
+        if cls:
+            return cls(pipeline_response, deserialized, {})  # type: ignore
+
+        return deserialized  # type: ignore
+
+    @distributed_trace_async
+    async def get_security_txt(self, **kwargs: Any) -> str:
+        """Get Security Txt.
+
+        Get the security.txt file.
+
+        :return: str
+        :rtype: str
+        :raises ~azure.core.exceptions.HttpResponseError:
+        """
+        error_map: MutableMapping = {
+            401: ClientAuthenticationError,
+            404: ResourceNotFoundError,
+            409: ResourceExistsError,
+            304: ResourceNotModifiedError,
+        }
+        error_map.update(kwargs.pop("error_map", {}) or {})
+
+        _headers = kwargs.pop("headers", {}) or {}
+        _params = kwargs.pop("params", {}) or {}
+
+        cls: ClsType[str] = kwargs.pop("cls", None)
+
+        _request = build_well_known_get_security_txt_request(
+            headers=_headers,
+            params=_params,
+        )
+        _request.url = self._client.format_url(_request.url)
+
+        _stream = False
+        pipeline_response: PipelineResponse = await self._client._pipeline.run(  # pylint: disable=protected-access
+            _request, stream=_stream, **kwargs
+        )
+
+        response = pipeline_response.http_response
+
+        if response.status_code not in [200]:
+            map_error(status_code=response.status_code, response=response, error_map=error_map)
+            raise HttpResponseError(response=response)
+
+        deserialized = self._deserialize("str", pipeline_response.http_response)
 
         if cls:
             return cls(pipeline_response, deserialized, {})  # type: ignore
