5877daa5c186da07a3962442d8479a1f226d8141
Ryunosuke O'Neil <r.oneil@cern.ch>
2026-01-22 14:17:15 +0100

    fix: guard against potential import error
    fix: format

--- diracx-core/src/diracx/core/models/__init__.py
+++ diracx-core/src/diracx/core/models/__init__.py
@@ -2,3 +2,11 @@
 for the DiracX API. They are shared between the client components (cli, api) and
 services components (db, logic, routers).
 """
+
+# in order to avoid DIRAC from failing to import TokenResponse
+# TODO: remove after DIRACGrid/DIRAC#8433
+from __future__ import annotations
+
+from .auth import TokenResponse
+
+__all__ = ["TokenResponse"]
