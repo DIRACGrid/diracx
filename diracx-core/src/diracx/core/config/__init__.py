a697eb6d25fe45bc138e091575abf53a3b188c99
AcquaDiGiorgio <64639359+AcquaDiGiorgio@users.noreply.github.com>
2026-04-10 15:23:48 +0200

    chore: Remove BaseModel from the public API

--- diracx-core/src/diracx/core/config/__init__.py
+++ diracx-core/src/diracx/core/config/__init__.py
@@ -4,7 +4,6 @@ from __future__ import annotations
 
 __all__ = [
     # Schema
-    "BaseModel",
     "Config",
     "DIRACConfig",
     "GroupConfig",
@@ -23,7 +22,6 @@ __all__ = [
 ]
 
 from .schema import (
-    BaseModel,
     Config,
     DIRACConfig,
     GroupConfig,
