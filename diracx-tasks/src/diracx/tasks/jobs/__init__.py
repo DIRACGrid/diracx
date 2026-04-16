c56460239e71597e330ed93ea714f434e931e538
AcquaDiGiorgio <64639359+AcquaDiGiorgio@users.noreply.github.com>
2026-04-16 11:27:26 +0200

    chore: update newly added __init__.py files

--- diracx-tasks/src/diracx/tasks/jobs/__init__.py
+++ diracx-tasks/src/diracx/tasks/jobs/__init__.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
-from .clean_sandbox_store import CleanSandboxStoreTask
+__all__ = [
+    "CleanSandboxStoreTask",
+]
 
-__all__ = ("CleanSandboxStoreTask",)
+from .clean_sandbox_store import CleanSandboxStoreTask
