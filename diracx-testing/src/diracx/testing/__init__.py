b69d85e17ae86d421d953957d8077295e3d65964
AcquaDiGiorgio <64639359+AcquaDiGiorgio@users.noreply.github.com>
2026-04-16 10:50:04 +0200

    fix: remove diracx-testing import changes

--- diracx-testing/src/diracx/testing/__init__.py
+++ diracx-testing/src/diracx/testing/__init__.py
@@ -1,3 +1,45 @@
 from __future__ import annotations
 
-__all__ = []
+__all__ = [
+    "verify_entry_points",
+    "ClientFactory",
+    "do_device_flow_with_dex",
+    "test_login",
+    "pytest_addoption",
+    "private_key",
+    "fernet_key",
+    "test_dev_settings",
+    "test_auth_settings",
+    "aio_moto",
+    "test_sandbox_settings",
+    "session_client_factory",
+    "client_factory",
+    "with_config_repo",
+    "demo_dir",
+    "demo_urls",
+    "demo_kubectl_env",
+    "cli_env",
+    "with_cli_login",
+]
+
+from .entrypoints import verify_entry_points
+from .utils import (
+    ClientFactory,
+    aio_moto,
+    cli_env,
+    client_factory,
+    demo_dir,
+    demo_kubectl_env,
+    demo_urls,
+    do_device_flow_with_dex,
+    fernet_key,
+    private_key,
+    pytest_addoption,
+    session_client_factory,
+    test_auth_settings,
+    test_dev_settings,
+    test_login,
+    test_sandbox_settings,
+    with_cli_login,
+    with_config_repo,
+)
