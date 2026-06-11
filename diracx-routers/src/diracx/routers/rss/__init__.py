9c375e28d565df13eae7a1de730c4f14357a3c01
Chris Burr <christopher.burr@cern.ch>
2026-06-11 09:40:41 +0200

    chore: simplify rss router module

--- diracx-routers/src/diracx/routers/rss/__init__.py
+++ diracx-routers/src/diracx/routers/rss/__init__.py
@@ -2,9 +2,5 @@ from __future__ import annotations
 
 __all__ = ["RSSAccessPolicy", "router"]
 
-from ..fastapi_classes import DiracxRouter
 from .access_policies import RSSAccessPolicy
-from .rss import router as rss_router
-
-router = DiracxRouter()
-router.include_router(rss_router)
+from .rss import router
